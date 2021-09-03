e67938779b8e83d2c5a720b67f50e78b5f6dead4
luxmiyu <luxmiyu.dev@gmail.com>
2021-09-03 18:35:03 +0200

    added audio plugins

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from cx_Freeze import setup, Executable
 build_options = {
     'packages': [],
     'excludes': ["tkinter"],
-    'include_files': ["resources"],
+    'include_files': ["resources", "plugins"],
     'zip_include_packages': 'PyQt5',
 }
 
@@ -15,7 +15,7 @@ executables = [
 ]
 
 setup(
-    name='template',
+    name='bpmcalc',
     version = '1.0.727',
     description = '',
     options = {'build_exe': build_options},
